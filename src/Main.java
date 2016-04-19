7c29eab3bb4cdb2302dc88f45a93c9d8f2970a38
Huy Nguyen <qhuydtvt@gmail.com>
2016-04-19 17:42:31 +0700

    Session 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
-
+    private static GameWindow gameWindow;
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        gameWindow = new GameWindow();
+        gameWindow.startThread();
     }
 }
